5bfc5435fb112c57e2537f44257032fc114a8441
Steve HOLWEG <skytep@gmail.com>
2018-08-23 18:53:06 +0200

    Working date

--- clock/src/main.cpp
+++ clock/src/main.cpp
@@ -73,29 +73,28 @@ void initDigits()
   //     D
   // * S
 
-
-  arraySegment[0] = 0x7E; // A B C D E F
-  arraySegment[1] = 0x30; // B c
-  arraySegment[2] = 0x6D; // A B D E G
-  arraySegment[3] = 0x79; // A B C D G
-  arraySegment[4] = 0x33; // B C F G
-  arraySegment[5] = 0x5B; // A C D F G
-  arraySegment[6] = 0x5F; // A C D E F G
-  arraySegment[7] = 0x70; // A B C
-  arraySegment[8] = 0x7F; // A B C D E F G
-  arraySegment[9] = 0x7B; // A B C D F G
+  arraySegment[0] = 0x7E;  // A B C D E F
+  arraySegment[1] = 0x30;  // B c
+  arraySegment[2] = 0x6D;  // A B D E G
+  arraySegment[3] = 0x79;  // A B C D G
+  arraySegment[4] = 0x33;  // B C F G
+  arraySegment[5] = 0x5B;  // A C D F G
+  arraySegment[6] = 0x5F;  // A C D E F G
+  arraySegment[7] = 0x70;  // A B C
+  arraySegment[8] = 0x7F;  // A B C D E F G
+  arraySegment[9] = 0x7B;  // A B C D F G
   arraySegment[10] = 0x00; // None = Off
 
-  arraySegmentReverse[0] = 0x7E; // A B C D E F
-  arraySegmentReverse[1] = 0x06; // E F
-  arraySegmentReverse[2] = 0x6D; // A B D E G
-  arraySegmentReverse[3] = 0x4F; // A D E F G
-  arraySegmentReverse[4] = 0x17; // C E F G
-  arraySegmentReverse[5] = 0x5B; // A C D F G
-  arraySegmentReverse[6] = 0x7B; // A B C D F G
-  arraySegmentReverse[7] = 0x0E; // D E F
-  arraySegmentReverse[8] = 0x7F; // A B C D E F G
-  arraySegmentReverse[9] = 0x5F; // A C D E F G
+  arraySegmentReverse[0] = 0x7E;  // A B C D E F
+  arraySegmentReverse[1] = 0x06;  // E F
+  arraySegmentReverse[2] = 0x6D;  // A B D E G
+  arraySegmentReverse[3] = 0x4F;  // A D E F G
+  arraySegmentReverse[4] = 0x17;  // C E F G
+  arraySegmentReverse[5] = 0x5B;  // A C D F G
+  arraySegmentReverse[6] = 0x7B;  // A B C D F G
+  arraySegmentReverse[7] = 0x0E;  // D E F
+  arraySegmentReverse[8] = 0x7F;  // A B C D E F G
+  arraySegmentReverse[9] = 0x5F;  // A C D E F G
   arraySegmentReverse[10] = 0x00; // None = Off
 
   float baseDelay = 0.1;
@@ -110,7 +109,7 @@ void initDigits()
   arraySegmentDelay[7] = (baseFactor - 3) * baseDelay; // D E F
   arraySegmentDelay[8] = (baseFactor - 7) * baseDelay; // A B C D E F G
   arraySegmentDelay[9] = (baseFactor - 6) * baseDelay; // A C D E F G
-  arraySegmentDelay[10] = 0 * baseDelay; // None = OffOff
+  arraySegmentDelay[10] = 0 * baseDelay;               // None = OffOff
 
   arrayDigit[0] = 0x8;  // 1000 INH
   arrayDigit[1] = 0x5;  //
@@ -121,7 +120,6 @@ void initDigits()
   arrayDigit[6] = 0x0;  //
   arrayDigit[7] = 0x4;  //
   arrayDigit[8] = 0x2;  //
-
 }
 
 //Display brightness
@@ -139,32 +137,33 @@ void initDigits()
 //50 dim but readable (0.56mA)
 //5 dim but readable (0.31mA)
 //1 dim but readable in dark (0.28mA)
-#define DISPLAY_BRIGHTNESS  300
-#define DISPLAY_BRIGHTNESS_SECOND  DISPLAY_BRIGHTNESS*0.5
+#define DISPLAY_BRIGHTNESS 300
+#define DISPLAY_BRIGHTNESS_SECOND DISPLAY_BRIGHTNESS * 0.5
 
-#define DIGIT_ON  HIGH
-#define DIGIT_OFF  LOW
+#define DIGIT_ON HIGH
+#define DIGIT_OFF LOW
 
-#define SEGMENT_ON  LOW
+#define SEGMENT_ON LOW
 #define SEGMENT_OFF HIGH
 
 // set digit, return if digit is reversed
-bool lightDigit(int digitToDisplay) {
+bool lightDigit(int digitToDisplay)
+{
 
   digitalWrite(digit0, (arrayDigit[digitToDisplay] & AddDigit0) ? DIGIT_ON : DIGIT_OFF);
   digitalWrite(digit1, (arrayDigit[digitToDisplay] & AddDigit1) ? DIGIT_ON : DIGIT_OFF);
   digitalWrite(digit2, (arrayDigit[digitToDisplay] & AddDigit2) ? DIGIT_ON : DIGIT_OFF);
   digitalWrite(digit3, (arrayDigit[digitToDisplay] & AddDigit3) ? DIGIT_ON : DIGIT_OFF);
 
-  return  (arrayDigit[digitToDisplay] & AddRecerse);
-
+  return (arrayDigit[digitToDisplay] & AddRecerse);
 }
 
 //Given a number, turns on those segments
 //If number == 10, then turn off number
-void lightNumber(int numberToDisplay, bool dot, bool seconds, bool reverse) {
+void lightNumber(int numberToDisplay, bool dot, bool seconds, bool reverse)
+{
 
-  unsigned int* digit = (reverse) ? arraySegmentReverse : arraySegment;
+  unsigned int *digit = (reverse) ? arraySegmentReverse : arraySegment;
 
   digitalWrite(segA, (digit[numberToDisplay] & AddA) ? SEGMENT_ON : SEGMENT_OFF);
   digitalWrite(segB, (digit[numberToDisplay] & AddB) ? SEGMENT_ON : SEGMENT_OFF);
@@ -176,14 +175,15 @@ void lightNumber(int numberToDisplay, bool dot, bool seconds, bool reverse) {
 
   digitalWrite(segP, (dot) ? SEGMENT_ON : SEGMENT_OFF);
   digitalWrite(segS, (seconds) ? SEGMENT_ON : SEGMENT_OFF);
-
 }
 
-void displayNumber(int toDisplay, int second) {
+void displayNumber(int toDisplay, int second)
+{
 
   long beginTime = millis();
 
-  for (int digit = 4 ; digit > 0 ; digit--) {
+  for (int digit = 4; digit > 0; digit--)
+  {
 
     //Turn on the right segments for this digit
     lightNumber(toDisplay % 10, false, false, lightDigit(digit));
@@ -200,7 +200,8 @@ void displayNumber(int toDisplay, int second) {
     lightDigit(0);
   }
 
-  for (int digit = 8 ; digit > 4 ; digit--) {
+  for (int digit = 8; digit > 4; digit--)
+  {
 
     //Turn on the right segments for this digit
     lightNumber(second % 10, false, false, lightDigit(digit));
@@ -217,78 +218,84 @@ void displayNumber(int toDisplay, int second) {
     lightDigit(0);
   }
 
-  while ( (millis() - beginTime) < 10) ;
+  while ((millis() - beginTime) < 10)
+    ;
   //Wait for 20ms to pass before we paint the display again
 }
 
-time_t UpdateClock() {
+time_t UpdateClock()
+{
 
-DateTime base = Clock.now();
-// By default add timezone
-  DateTime future (base + TimeSpan(0, TimeZone, 0, 0));
-  
+  DateTime base = Clock.now();
+  // By default add timezone
+  DateTime future(base + TimeSpan(0, TimeZone, 0, 0));
+
   bool dst = false;
 
   int thisMonth = future.month();
   int thisDay = future.day();
   int thisWeekday = future.dayOfTheWeek();
   int thisHour = future.hour();
-  
-  if(thisMonth == 10 && thisDay < (thisWeekday + 24))
+
+  if (thisMonth == 10 && thisDay < (thisWeekday + 24))
   {
-    dst=true;
+    dst = true;
   }
 
-  if(thisMonth == 10 && thisDay > 24 && thisWeekday == 1 && thisHour < 2)
+  if (thisMonth == 10 && thisDay > 24 && thisWeekday == 1 && thisHour < 2)
   {
-    dst=true;
+    dst = true;
   }
 
-  if(thisMonth < 10 && thisMonth > 3) dst = true;
-  
-  if(thisMonth == 3 && thisDay > 24 && thisDay >= (thisWeekday + 24))
+  if (thisMonth < 10 && thisMonth > 3)
+    dst = true;
+
+  if (thisMonth == 3 && thisDay > 24 && thisDay >= (thisWeekday + 24))
   {
-    if(!(thisWeekday == 1 && thisHour < 2)) dst = true;
+    if (!(thisWeekday == 1 && thisHour < 2))
+      dst = true;
   }
-  
+
   // if dst, add one hour
-  if(dst) future = (future + TimeSpan(0, 1, 0, 0)); 
-  
-Serial.println("Date update !");
+  if (dst)
+    future = (future + TimeSpan(0, 1, 0, 0));
 
-tmElements_t tmSet;
-tmSet.Day = future.day();
-tmSet.Hour = future.hour();
-tmSet.Minute = future.minute();
-tmSet.Month = future.month();
-tmSet.Second = future.second();
-tmSet.Wday = future.dayOfTheWeek();
-tmSet.Year = future.year();
+  Serial.println("Date update !");
 
-return makeTime(tmSet);
+  tmElements_t tmSet;
+  tmSet.Day = future.day();
+  tmSet.Hour = future.hour();
+  tmSet.Minute = future.minute();
+  tmSet.Month = future.month();
+  tmSet.Second = future.second();
+  tmSet.Wday = future.dayOfTheWeek();
+  tmSet.Year = CalendarYrToTm(future.year());
 
-
+  return makeTime(tmSet);
 }
-
 
 // 1-Wire network
 
-void receiveData(int byteCount) {
-  while (Wire.available()) {
+void receiveData(int byteCount)
+{
+  while (Wire.available())
+  {
     dataReceived = Wire.read();
     Serial.print("Donnee recue : ");
     Serial.println(dataReceived);
   }
 }
 
-void sendData() {
+void sendData()
+{
   int envoi = dataReceived + 1;
   Wire.write(envoi);
 }
 
 // End 1-Wire network
 
-void setup() {
+void setup()
+{
   initDigits();
 
   pinMode(segA, OUTPUT);
@@ -313,39 +320,41 @@ void setup() {
   Wire.onRequest(sendData);
   Serial.println("Ready!");
 
-
-  if (! Clock.begin()) {
+  if (!Clock.begin())
+  {
     Serial.println("Couldn't find RTC");
-    while (1);
+    while (1)
+      ;
   }
 
-  if (Clock.lostPower()) {
+  if (Clock.lostPower())
+  {
     Serial.println("RTC lost power, lets set the time!");
   }
 
   setSyncProvider(UpdateClock);
-  
 }
 
-void digitalClockDisplay(){
+void digitalClockDisplay()
+{
   // digital clock display of the time
   Serial.print(hour());
-  Serial.print(" ");
+  Serial.print(":");
   Serial.print(minute());
-  Serial.print(" ");
+  Serial.print(":");
   Serial.print(second());
   Serial.println();
   Serial.print(day());
-  Serial.print(" ");
+  Serial.print("/");
   Serial.print(month());
-  Serial.print(" ");
-  Serial.print(year()); 
-  Serial.println(); 
+  Serial.print("/");
+  Serial.print(year());
+  Serial.println();
 }
 
-void loop() {
+void loop()
+{
 
-//digitalClockDisplay();
   //displayNumber(1234, 5678);
-  displayNumber(hour()*100+minute(), (millis() / 1000) + 1000);
+  displayNumber(hour() * 100 + minute(), (millis() / 1000) + 1000);
 }
